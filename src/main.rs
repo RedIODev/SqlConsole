1c393b26a34e5418546d72a6d11bdf104abd5ab1
Luca Lehmann <luca.lehmann@ppcschule.onmicrosoft.com>
2022-05-17 10:10:09 +0200

    started console impl

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use sql_console::console::constants::prompt;
 
+use rpassword::read_password;
+use rusqlite::Connection;
 
-
-fn main()  {
-    println!("{}",prompt("DemoUser","DemoServer","~"));
+fn main() -> Result<(),TError> {
+    let (username, hostname) = parse_env_args(std::env::args().collect())?;
+    print!("{}", password_prompt(&username, &hostname));
+    let password = read_password()?;
+    loop {
+        println!("{}",prompt(&username,&hostname,"~"));
+        
+    }
+    Ok(())
 }
