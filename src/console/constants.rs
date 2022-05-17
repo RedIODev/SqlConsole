3a1d20b4cc6e1d5a4939a809ff16ae183c73956a
Luca Lehmann <luca.lehmann@ppcschule.onmicrosoft.com>
2022-05-17 10:11:00 +0200

    started console impl

--- src/console/constants.rs
+++ src/console/constants.rs
@@ -1,14 +1,5 @@
-use const_format::concatcp;
-
 use crate::set_foreground_color;
 
-const COLOR_USER:&str = set_foreground_color!(22,198,12);
-const COLOR_DB:&str = set_foreground_color!(59,120,255);
-const COLOR_NORMAL:&str = set_foreground_color!(204,204,204);
-
-pub fn prompt(username: &str,hostname:&str,database:&str) -> String {
-    format!("{}{}@{}{}:{}{}{}$ ",COLOR_USER,username,hostname,COLOR_NORMAL,COLOR_DB,database,COLOR_NORMAL)
-}
-
-pub const PROMPT: &str = concatcp!(COLOR_USER, "{}@{}", COLOR_NORMAL, ":", COLOR_DB ,"{}", COLOR_NORMAL, "$ "); //username, hostname, database
-pub const PASS_PROMPT: &str = concatcp!(COLOR_NORMAL, "{}@{}'s password: "); //username, hostname
+pub const COLOR_USER:&str = set_foreground_color!(22,198,12);
+pub const COLOR_DB:&str = set_foreground_color!(59,120,255);
+pub const COLOR_NORMAL:&str = set_foreground_color!(204,204,204);
--- src/console/mod.rs
+++ src/console/mod.rs
@@ -1,2 +1,42 @@
-pub mod constants;
+
+use std::{error::Error, fmt::Display};
+
+use self::constants::{COLOR_DB, COLOR_NORMAL, COLOR_USER};
+
 pub mod ansi;
+pub mod constants;
+
+pub fn prompt(username: &str, hostname: &str, database: &str) -> String {
+    format!(
+        "{}{}@{}{}:{}{}{}$ ",
+        COLOR_USER, username, hostname, COLOR_NORMAL, COLOR_DB, database, COLOR_NORMAL
+    )
+}
+
+pub fn password_prompt(username: &str, hostname: &str) -> String {
+    format!("{}{}@{}'s password: ", COLOR_NORMAL, username, hostname)
+}
+
+pub type TError = Box<dyn Error>;
+#[derive(Debug)]
+pub struct EnvError(String);
+
+impl EnvError {
+    pub fn from(err_string: String) -> Box<EnvError> {
+        Box::new(EnvError(err_string))
+    }
+}
+
+impl Display for EnvError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f,"{}",self.0)
+    }
+}
+
+impl Error for EnvError {}
+
+pub fn parse_env_args(mut args: Vec<String>) -> Result<(String, String),TError> {
+    let len = args.len();
+    if len != 3 { return Err(EnvError::from(format!("Invalid argument length: {}, only 3 are permitted",len)))}
+    Ok((args.remove(1),args.remove(1)))
+}
